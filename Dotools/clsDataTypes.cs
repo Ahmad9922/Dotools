e672748a70d8234224526d73c2ee6c65f422f396
Ahmad Albakkai <desoograph55j@gmail.com>
2025-05-25 17:49:57 +0300

    Initial commit - still in development

---
+++ Dotools/clsDataTypes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotools
+{
+    public static class clsDataTypes
+    {
+        public class clsFilterData
+        {
+            public enum enFilterStyle { Contains = 1, Equals = 2 }
+
+            public string Value { get; set; }
+            public string FieldName { get; set; }
+            public enFilterStyle FilterStyle { get; set; }
+
+            public clsFilterData()
+            {
+
+            }
+
+            public clsFilterData(string Value, string FieldName)
+            {
+                this.Value = Value;
+                this.FieldName = FieldName;
+                FilterStyle = enFilterStyle.Contains;
+            }
+        }
+    }
+}
---
+++ Dotools/clsEventLogger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotools
+{
+    public class clsEventLogger
+    {
+        public static void WriteEntry(string LogName, string SourceName, string Message, EventLogEntryType Type)
+        {
+            if (!string.IsNullOrEmpty(LogName) && !string.IsNullOrEmpty(SourceName))
+            {
+                try
+                {
+                    using (EventLog eventLog = new EventLog(LogName))
+                    {
+                        eventLog.Source = SourceName;
+
+                        if (!EventLog.SourceExists(eventLog.Source))
+                        {
+                            EventLog.CreateEventSource(eventLog.Source, LogName);
+                        }
+
+                        eventLog.WriteEntry(Message, Type);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// This method sets the source using the current Process name.
+        /// </summary>
+        public static void WriteEntryInApplicationLog(string Message, EventLogEntryType Type)
+        {
+            WriteEntry("Application", Process.GetCurrentProcess().ProcessName, Message, Type);
+        }
+
+        public static void WriteEntryInApplicationLog(string SourceName, string Message, EventLogEntryType Type)
+        {
+            WriteEntry("Application", SourceName, Message, Type);
+        }
+    }
+}
